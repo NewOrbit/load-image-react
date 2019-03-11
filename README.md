f4f6b0508ecef3d76374ccf004112623542d789c
James Monger <jameskmonger@hotmail.co.uk>
2019-03-11 13:51:26 +0000

    Implement react-load-image

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # react-load-image
-Use blueimp-load-image in React
+
+Use [blueimp-load-image](https://www.npmjs.com/package/blueimp-load-image) in React
+
+## Installation
+
+    npm install react-load-image
+
+## Usage
+
+    import { Image } from "react-load-image";
+
+    const SomeComponent = () => {
+        return (
+            <>
+                <h2>My Image</h2>
+
+                <Image
+                    src="some-image-url.png"
+                    loadOptions={{
+                        downsamplingRatio: 0.5,
+                        maxWidth: 200,
+                        maxHeight: 200
+                    }}
+                >
+            </>
+        );
+    };
+
+### Load options
+
+The `loadOptions` prop is passed into [blueimp-load-image](https://www.npmjs.com/package/blueimp-load-image#options). `canvas` and `orientation` will always be set to `true`.
+
+## License
+
+Made with :sparkling_heart: by [NewOrbit](https://www.neworbit.co.uk/) in Oxfordshire, and licensed under the [MIT License](LICENSE)
---
+++ src/image.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { loadImageToCanvas, LoadImageOptions } from "./load-image-to-canvas";
+
+interface ImageProps {
+    src: string;
+
+    loadOptions?: LoadImageOptions;
+}
+
+export class Image extends React.Component<ImageProps> {
+    private container: HTMLAnchorElement;
+
+    public async componentDidMount() {
+        await this.renderImage();
+    }
+
+    public async componentDidUpdate(prevProps: ImageProps) {
+        if (this.props.src === prevProps.src) {
+            return;
+        }
+
+        await this.renderImage();
+    }
+
+    public render() {
+        return <a ref={el => this.container = el} href={this.props.src} className="loaded-image-container" />;
+    }
+
+    private async renderImage() {
+        while (this.container.firstChild) {
+            this.container.removeChild(this.container.firstChild);
+        }
+
+        const canvas = await loadImageToCanvas(this.props.src, this.props.loadOptions);
+
+        this.container.appendChild(canvas);
+    }
+}
