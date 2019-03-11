caba659d6ae6d764f5106b917f48d7af0d2b0146
James Monger <jameskmonger@hotmail.co.uk>
2019-03-11 14:46:04 +0000

    Implement react-load-image (#1)
    * Implement react-load-image
    
    * Code review changes

---
+++ src/load-image-to-canvas.ts
@@ -0,0 +1,48 @@
+import loadImage = require("blueimp-load-image");
+
+export interface LoadImageOptions {
+    canvas?: true;
+    orientation?: true;
+
+    maxWidth?: number;
+    maxHeight?: number;
+    minWidth?: number;
+    minHeight?: number;
+    sourceWidth?: number;
+    sourceHeight?: number;
+    top?: number;
+    right?: number;
+    bottom?: number;
+    left?: number;
+    contain?: boolean;
+    cover?: boolean;
+    aspectRatio?: number;
+    pixelRatio?: number;
+    downsamplingRatio?: number;
+    crop?: boolean;
+    crossOrigin?: boolean;
+}
+
+const defaultOptions: LoadImageOptions = {
+    canvas: true,
+    orientation: true
+};
+
+const getOptions = (options: LoadImageOptions = {}) => {
+    return {
+        ...options,
+        ...defaultOptions
+    };
+};
+
+export const loadImageToCanvas = (src: string, options: LoadImageOptions) => {
+    const opts = getOptions(options);
+
+    return new Promise<HTMLCanvasElement>(resolve => {
+        loadImage(
+            src,
+            (canvas: HTMLCanvasElement) => resolve(canvas),
+            opts
+        );
+    });
+};
